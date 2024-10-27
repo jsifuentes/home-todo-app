2144aa6cc277e88023715f29f37d782384dd6e4e
Jacob Sifuentes <jacob@sifuen.com>
2024-10-27 13:42:18 -0400

    deploy script

--- my-deploy-script.sh
+++ my-deploy-script.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# check if working tree has changes
-if [[ -n $(git status -s) ]]; then
-	echo "Working tree has changes. Please commit or stash your changes before deploying."
-	exit 1
-fi
-
-# make sure we have no commits to push up
-if [[ -n $(git branch --show-current) ]]; then
+if [[ -n $(git cherry -v) ]]; then
 	echo "You have unpushed commits. Please push your commits before deploying."
 	exit 1
 fi
 
-ssh user@server.lab.sifuen.com "cd ~/Documents/homelab/todo/ && git pull && docker compose stop todo && docker compose up --build -d todo"
+ssh-add ~/.ssh/id_rsa
+ssh -A user@server.lab.sifuen.com "cd ~/Documents/homelab/todo/ && git pull && docker compose stop todo && docker compose up --build -d todo"
