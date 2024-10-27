c086f89141206b8aeb5d1c1a5928b5398ac0fe33
Jacob Sifuentes <jacob@sifuen.com>
2024-10-27 12:54:20 -0400

    favicon updates

--- src/web/index.php
+++ src/web/index.php
@@ -19,6 +19,12 @@ require_once __DIR__ . '/../init.php';
 	<script src="https://unpkg.com/htmx.org@1.9.12/dist/ext/alpine-morph.js"></script>
 	<script src="/assets/js/Sortable.js"></script>
 	<script src="/assets/js/tasksList.js"></script>
+
+	<link href='/apple-touch-icon.png' rel='apple-touch-icon' type='image/png'>
+	<link rel="manifest" href="/assets/site.webmanifest">
+	<link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
+	<link rel="shortcut icon" href="/favicon.ico">
 </head>
 
 <body class="bg-gray-100 lg:p-8 p-2">
