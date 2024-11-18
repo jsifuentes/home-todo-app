35a0e2111c278ade9da001fd9a18a0dbc9768985
Jacob Sifuentes <jacob@sifuen.com>
2024-11-18 13:06:26 -0500

    fix: cron schedule

--- src/cron/create_tasks_from_recurring.php
+++ src/cron/create_tasks_from_recurring.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../init.php';
 
 $now = datetimeToLocalDateTime('now');
 $currentHour = (int)$now->format('G');
-if ($currentHour !== 18) {
+if ($currentHour !== 9) {
 	exit("Not running - current hour is {$currentHour}, waiting for 9\n");
 }
 
